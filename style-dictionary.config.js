02371ef84ec93102267b186368568dd5dfe95a97
Jeff Wade <jeffwade@users.noreply.github.com>
2026-06-06 13:26:05 -0700

    scaffold astro migration

---
+++ style-dictionary.config.js
@@ -0,0 +1,19 @@
+export default {
+  source: ['tokens/tokens.json'],
+  platforms: {
+    css: {
+      transformGroup: 'css',
+      buildPath: 'src/styles/',
+      files: [
+        {
+          destination: 'tokens.css',
+          format: 'css/variables',
+          options: {
+            outputReferences: true,
+            selector: ':root',
+          },
+        },
+      ],
+    },
+  },
+};
